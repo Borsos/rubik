eae909ec179e8caa6267e13b3e01f0d102966b6c
Simone Campagna <simone.campagna@tiscali.it>
2014-03-26 13:09:15 +0100

    Preparing 1.0.27

--- lib/python/rubik/conf.py
+++ lib/python/rubik/conf.py
@@ -30,7 +30,7 @@ from .units import Memory
 
 VERSION_MAJOR = 1
 VERSION_MINOR = 0
-VERSION_PATCH = 26
+VERSION_PATCH = 27
 VERSION = "{major}.{minor}.{patch}".format(
     major=VERSION_MAJOR,
     minor=VERSION_MINOR,
