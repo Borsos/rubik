092fe325f6fe5bf522ece5ea1466f21f84f79552
Simone Campagna <simone.campagna@tiscali.it>
2014-08-18 22:38:16 +0200

    bug fix

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(
     author = "Simone Campagna",
     author_email = "simone.campagna@tiscali.it",
     url="https://github.com/simone-campagna/rubik",
-    packages = ["rubik", "rubik.application", "rubik.cubes", "rubik.viewer", "rubik.viewer.impl"],
+    packages = ["rubik", "rubik.application", "rubik.cubes", "rubik.visualizers", "rubik.visualizers.impl"],
     scripts = scripts,
     package_data = {},
 )
