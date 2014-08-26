7f0d3bf3dceb6054a22259dbef87955a459bc4ba
Simone Campagna <simone.campagna@tiscali.it>
2014-08-26 10:16:20 +0200

    added test for read/write attributes

--- testing/TESTS/expr.sh
+++ testing/TESTS/expr.sh
@@ -41,13 +41,13 @@ check_files_are_equal a_${X}x${Y}.raw c_${X}x${Y}.raw
 
 expr_file="expr.w"
 cat > "${expr_file}" << EOFCAT
-a = cb.const_cube("${X}x${Y}", 2)
-write_cube(filename="c2_{shape}.{format}", cube=a)
-b = cb.const_cube("${X}x${Y}", 3)
-write_cube(filename="c3_{shape}.{format}", cube=b)
-c = cb.const_cube("${X}x${Y}", 5)
-write_cube(filename="c5_{shape}.{format}", cube=c)
-write_cube(filename="c2+3_{shape}.{format}", cube=a + b)
+a = cb.const_cube('${X}x${Y}', 2)
+write_cube(filename='c2_{shape}.{format}', cube=a)
+b = cb.const_cube('${X}x${Y}', 3)
+write_cube(filename='c3_{shape}.{format}', cube=b)
+c = cb.const_cube('${X}x${Y}', 5)
+write_cube(filename='c5_{shape}.{format}', cube=c)
+write_cube(filename='c2+3_{shape}.{format}', cube=a + b)
 EOFCAT
 
 test_prex "-f ${expr_file}"
@@ -60,9 +60,9 @@ check_files_are_not_equal c2_${X}x${Y}.raw c5_${X}x${Y}.raw
 
 expr_file="expr.r"
 cat > "${expr_file}" << EOFCAT
-a = read_cube(filename="c2_{shape}.{format}", shape="${X}x${Y}")
-b = read_cube(filename="c3_{shape}.{format}", shape="${X}x${Y}")
-write_cube(filename="c5bis_{shape}.{format}", cube=a + b)
+a = read_cube(filename='c2_{shape}.{format}', shape='${X}x${Y}')
+b = read_cube(filename='c3_{shape}.{format}', shape='${X}x${Y}')
+write_cube(filename='c5bis_{shape}.{format}', cube=a + b)
 EOFCAT
 
 test_prex "-f ${expr_file}"
@@ -71,14 +71,19 @@ check_files_are_equal c5bis_${X}x${Y}.raw c5_${X}x${Y}.raw
 
 expr_file="expr.x"
 cat > "${expr_file}" << EOFCAT
-a = read_cube(filename="c2_{shape}.{format}", shape="${X}x${Y}", extractor=":$EX,:$EY")
-b = read_cube(filename="c3_{shape}.{format}", shape="${X}x${Y}", extractor=":$EX,:$EY")
-write_cube(filename="c5a_{shape}.{format}", cube=a + b)
-c = read_cube(filename="c5_{shape}.{format}", shape="${X}x${Y}", extractor=":$EX,:$EY")
-write_cube(filename="c5b_{shape}.{format}", cube=c)
+a = read_cube(filename='c2_{shape}.{format}', shape='${X}x${Y}', extractor=':$EX,:$EY')
+b = read_cube(filename='c3_{shape}.{format}', shape='${X}x${Y}', extractor=':$EX,:$EY')
+write_cube(filename='c5ab_{shape}.{format}', cube=a + b)
+c = read_cube(filename='c5_{shape}.{format}', shape='${X}x${Y}', extractor=':$EX,:$EY')
+write_cube(filename='c5c_{shape}.{format}', cube=c)
+write_cube(filename='c5d_{shape}.{format}', cube=c, format='text')
+e = read_cube(filename='c5d_{shape}.{format}', format='text', shape='${EX}x${EY}')
+write_cube(filename='c5e_{shape}.{format}', cube=e, format='raw')
 EOFCAT
 
 test_prex "-f ${expr_file}"
-check_file_exists_and_has_size c5a_${EX}x${EY}.raw $(( $EX * $EY * ${bytes_float32} ))
-check_file_exists_and_has_size c5b_${EX}x${EY}.raw $(( $EX * $EY * ${bytes_float32} ))
-check_files_are_equal c5a_${EX}x${EY}.raw c5b_${EX}x${EY}.raw
+check_file_exists_and_has_size c5ab_${EX}x${EY}.raw $(( $EX * $EY * ${bytes_float32} ))
+check_file_exists_and_has_size c5c_${EX}x${EY}.raw $(( $EX * $EY * ${bytes_float32} ))
+check_files_are_equal c5ab_${EX}x${EY}.raw c5c_${EX}x${EY}.raw
+check_file_exists c5d_${EX}x${EY}.text
+check_files_are_equal c5e_${EX}x${EY}.raw c5c_${EX}x${EY}.raw
