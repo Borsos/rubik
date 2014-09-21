6aa2ee3eb10693190f92f72d17d77c3e8e4fe3b7
Simone Campagna <simone.campagna@tiscali.it>
2014-09-21 14:23:57 +0200

    bug fixing

--- testing/setup.py
+++ testing/setup.py
@@ -25,9 +25,7 @@ scripts = [
 ]
 
 dirname = os.path.dirname(os.path.abspath(sys.argv[0]))
-print dirname
 py_dirname = os.path.join(dirname, "..")
-print py_dirname
 sys.path.insert(0, py_dirname)
 
 from rubik import conf
